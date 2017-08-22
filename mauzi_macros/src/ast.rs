eb56045ef2a84372d3fe93f65b549adcf1737ab9
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2017-08-22 11:06:09 +0200

    Add documentation and comments to `ast` and `lib`

--- mauzi_macros/src/ast.rs
+++ mauzi_macros/src/ast.rs
@@ -1,18 +1,37 @@
+//! This module defines all types that make up the AST.
+//!
+//! The way the macro works is not very different from how many compilers work,
+//! though a lot simpler:
+//!
+//! ```
+//! ┌─────────────┐     ┌───────┐     ┌─────────────┐
+//! │ TokenStream │  ➡  │  AST  │  ➡  │ TokenStream │
+//! └─────────────┘     └───────┘     └─────────────┘
+//!                  ↑             ↑
+//!                parse          gen
+//! ```
+//!
+//! So the AST is the abstract representation of the dictonary after parsing.
+//!
+
+
 use std::ops::Deref;
 use proc_macro::{Span, Term, TokenNode, TokenStream, TokenTree};
 
 
+/// A dictonary, consisting of zero or more *translation units*.
 #[derive(Debug, Clone)]
 pub struct Dict {
-    // pub header: DictHeader,
     pub trans_units: Vec<TransUnit>,
 }
 
-#[derive(Debug, Clone)]
-pub struct DictHeader {
-    pub dict_name: Ident,
-}
-
+/// A named translation unit, consisting of a definition and optional
+/// parameters.
+///
+/// The name of a translation unit is commonly called *translation key*. The
+/// definition of a translation unit contains definitions for different
+/// languages. In those definitions, the translation unit's parameters may be
+/// used.
 #[derive(Debug, Clone)]
 pub struct TransUnit {
     pub name: Ident,
@@ -20,26 +39,68 @@ pub struct TransUnit {
     pub body: UnitBody,
 }
 
+/// A paramter of a translation unit.
+///
+/// # Example
+///
+/// ```
+/// name: &str
+/// ```
 #[derive(Debug, Clone)]
 pub struct UnitParam {
     pub name: Ident,
     pub ty: Ty,
 }
 
-#[derive(Debug, Clone)]
-pub struct Ty(pub String);
-
+/// The body of a translation unit, consisting of zero or more arms.
+///
+/// # Example
+///
+/// ```
+/// En(Gb) => "Hello sir",
+/// De => "Guten Tag",
+/// ```
 #[derive(Debug, Clone)]
 pub struct UnitBody {
     pub arms: Vec<UnitArm>,
 }
 
+/// One arm of a translation unit's body, consisting of a pattern/matcher and
+/// a body.
+///
+/// # Example
+///
+/// ```
+/// En(Gb) => "Hello sir"
+/// ```
 #[derive(Debug, Clone)]
 pub struct UnitArm {
     pub pattern: ArmPattern,
     pub body: ArmBody,
 }
 
+/// One arm's pattern.
+///
+/// # Example
+///
+/// Underscore/wildcard pattern:
+/// ```
+/// _
+/// ```
+///
+/// Only language given:
+/// ```
+/// En
+/// // ... or ...
+/// lang
+/// ```
+///
+/// Language and region given
+/// ```
+/// En(Gb)
+/// // ... or ...
+/// En(region)
+/// ```
 #[derive(Debug, Clone)]
 pub enum ArmPattern {
     Underscore,
@@ -59,12 +120,39 @@ impl ArmPattern {
     }
 }
 
+/// The body of one arm.
+///
+/// Right now we support two kinds of bodies:
+/// - String literals with placeholders
+/// - Raw Rust code
+///
+/// # Example
+///
+/// String literal:
+/// ```
+/// "Hallo {user}"
+/// ```
+///
+/// Raw Rust body:
+/// ```
+/// {
+///     format!("Hallo {}", user)
+/// }
+/// ```
 #[derive(Debug, Clone)]
 pub enum ArmBody {
     Str(String),
     Raw(TokenStream),
 }
 
+/// A Rust type.
+///
+/// Since we don't want to replicate the Rust type parsing algorithm, we are
+/// lazy and simply store all tokens as a string.
+#[derive(Debug, Clone)]
+pub struct Ty(pub String);
+
+/// An identificator (some word like thing).
 #[derive(Debug, Clone, Copy)]
 pub struct Ident {
     term: Term,
@@ -77,6 +165,9 @@ impl Ident {
         }
     }
 
+    /// Returns a token tree representing this ident in the same expansion
+    /// context as the macro invocation. This means that the user can use this
+    /// name.
     pub fn exported(&self) -> TokenTree {
         TokenTree {
             span: Span::call_site(),
@@ -84,6 +175,7 @@ impl Ident {
         }
     }
 
+    /// Intern the given string with `new()` and call `exported()`.
     pub fn export(s: &str) -> TokenTree {
         Self::new(s).exported()
     }
@@ -107,6 +199,9 @@ impl From<Term> for Ident {
 }
 
 impl Into<TokenStream> for Ident {
+    /// This implementation returns a token stream that represents this ident
+    /// in a macro internal expansion context. Thus the user cannot access this
+    /// name.
     fn into(self) -> TokenStream {
         TokenNode::Term(self.term).into()
     }
--- mauzi_macros/src/lib.rs
+++ mauzi_macros/src/lib.rs
@@ -1,3 +1,8 @@
+//! This crate defines the proc-macro `dict!`.
+//!
+//! You shouldn't use this crate directly, but use `mauzi` instead. The macro
+//! is reexported there.
+
 #![feature(proc_macro)]
 
 extern crate literalext;
@@ -6,13 +11,18 @@ extern crate mauzi_runtime;
 
 use std::result::Result as StdResult;
 
-
 mod ast;
 mod gen;
 mod parse;
 
+/// Right now the only way to report errors to the user from within a proc-
+/// macro is to panic, which prints a simple string. So right now our error
+/// type for everything contains a string as error value.
 type Result<T> = StdResult<T, String>;
 
+/// Generates a dictonary hosting translations in different languages.
+///
+/// **TODO**: documentation
 #[proc_macro]
 pub fn dict(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     parse::parse(input)
